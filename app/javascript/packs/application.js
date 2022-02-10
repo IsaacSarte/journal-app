6269bb409640c9e44d49235f0144908d002d6a77
Isaac Sarte <aizakku09@gmail.com>
2022-02-10 22:18:41 +0800

    landing page done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,13 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+window.onload = () => {
+    const logo = document.querySelectorAll('#logo path');
+
+    for(let i = 0; i < logo.length; i++) {
+        console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+    }
+}
+
+
