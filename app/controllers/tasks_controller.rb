9c99e60825c916885eacfcf52fcfcd4cffaf792a
Isaac Sarte <aizakku09@gmail.com>
2022-02-11 17:37:01 +0800

    adding categories done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,12 @@
 class TasksController < ApplicationController
     before_action :set_journal
     before_action :set_task, only: %i[show edit update destroy]
+
+    def index
+        redirect_to @journal
+    end
+
+    def show
+        @description = @task.descriptions.build
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,12 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   get '/journals' => 'journals#index'
+  get '/journals/new' => 'journals#new'
+
+  post '/journals' => 'journals#create', as: 'journal_create'
+
+  get '/journals/:id' => 'journals#show', as: 'journal'
+
+  get '/journals/:id/edit' => 'journals#edit', as: 'journal_edit'
 
 end
