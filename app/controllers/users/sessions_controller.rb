90fcbd076008ba0d037414be29c562d3aa14500c
Isaac Sarte <aizakku09@gmail.com>
2022-02-12 10:42:03 +0800

    removed flash

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -11,7 +11,6 @@ module Users
 
     def create
       self.resource = warden.authenticate!(auth_options)
-      flash[:notice] = I18n.t(:sign_in)
       sign_in(resource_name, resource)
       yield resource if block_given?
       respond_with resource, location: after_sign_in_path_for(resource)
@@ -20,7 +19,6 @@ module Users
     # DELETE /resource/sign_out
     def destroy
       signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-      flash[:notice] = I18n.t(:sign_out) if signed_out
       yield if block_given?
       respond_to_on_destroy
     end
