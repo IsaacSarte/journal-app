f7d66d674dd991177c9fb75a3d6f6fa1a5f1da3c
IsaacSarte <sarteisaac09@gmail.com>
2022-02-17 22:40:26 +0800

    added due date

--- spec/system/task_system_spec.rb
+++ spec/system/task_system_spec.rb
@@ -1,16 +1,40 @@
 require 'rails_helper'
 
 #new
-describe 'Get task' do
-    scenario 'valid task attributes' do
-        get '/journals/:journal_id/tasks/new', params: {
-            task: {
-                name: 'Test Task',
-                journal_id: ':journal_id',
-            }
+# describe 'Get task' do
+#     scenario 'valid task attributes' do
+#         post '/journals/:journal_id/tasks/new', params: {
+#             task: {
+#                 name: 'Test Task',
+#                 journal_id: ':journal_id',
+#             }
+#         }
+
+#         expect(response.status).to eq(302)
+#     end
+# end
+
+describe 'POST /tasks' do
+    scenario 'valid journal/category attributes' do
+        post '/journals', params: {
+            journal: { 
+                name: 'Test Category',
+                id: 69
+             }
         }
 
         expect(response.status).to eq(302)
+    end
+    scenario 'valid task attributes' do
+        post '/journals/:journal_id/tasks', params: {
+            journal: {
+                id: 69,
+                tasks: { 
+                    journal_id: 69,
+                    name: 'Test Task'
+                 }
+             }
+        }
     end
 end
 
